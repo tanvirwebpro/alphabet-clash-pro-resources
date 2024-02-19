98c1e7fe3badd0165f3bf5a3b6b3f7a5adcdfcd1
tanvirwebpro <tanvirahmed9878@gmail.com>
2024-02-19 07:58:23 +0600

    js file updated

--- scripts/aplha-clash.js
+++ scripts/aplha-clash.js
@@ -5,6 +5,66 @@
 //      const playGroundSection =document.getElementById('play-ground-section');
 //      playGroundSection.classList.remove('hidden');
 // }
+// function handleKeyboardButtonPress(){
+// console.log('button pressed');
+// }
+// document.addEventListener('keyup', handleKeyboardButtonPress)
+
+function handleKeyboardKeyupEvent(event){
+    const playerPressed =event.key;
+    console.log('player pressed',playerPressed);
+    const expectedAlphabet =document.getElementById('current-alphabet');
+    const alphabet = (expectedAlphabet.innerText).toLowerCase() ;
+    console.log(playerPressed,alphabet);
+
+    if(playerPressed === alphabet){
+        console.log('success');
+        // get the current score
+        const currentScoreElement =document.getElementById('current-score');
+        const currentScoreText =currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+        // increase the score by 1
+        const newScore = currentScore + 1;
+        // show the updated score
+        currentScoreElement.innerText=newScore;
+
+
+        removeBackgroundColorById(alphabet);
+        continueGame();
+    }
+    else {
+       const currentLifeElement =document.getElementById('current-life');
+       const currentLifeText =currentLifeElement.innerText;
+       const currentLife =parseInt(currentLifeText);
+       const newLife =currentLife -1;
+       currentLifeElement.innerText = newLife;
+    }
+}
+
+
+
+// function handleKeyboardKeyup(event){
+//     const playerPressed = event.key; 
+    
+//     const currentAlphabetElement =document.getElementById('current-alphabet');
+//     const currentAlphabet =currentAlphabetElement.innerText;
+//     const expectedAlphabet =currentAlphabet.toLowerCase();
+//     console.log(playerPressed , expectedAlphabet);
+
+//     if( playerPressed === expectedAlphabet){
+//         console.log('score +');
+//     }
+//     else{
+//         console.log('minimize life point')
+//     }
+
+
+
+
+// }
+
+document.addEventListener('keyup',handleKeyboardKeyupEvent);
 
 function continueGame(){
     // generate a random  alphabet
--- scripts/utility.js
+++ scripts/utility.js
@@ -10,6 +10,10 @@ function setBackgroundColorById(elementId){
     const element =document.getElementById(elementId);
     element.classList.add('bg-orange-500')
 }
+function removeBackgroundColorById(elementId){
+    const element =document.getElementById(elementId);
+    element.classList.remove('bg-orange-500')
+}
 
 
 function getARandomAlphabet(){
